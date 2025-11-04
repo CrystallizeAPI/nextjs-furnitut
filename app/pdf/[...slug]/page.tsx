e0e47b383ae0bd9747df2cd17c9e6827e4d0c796
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-11-04 13:04:13 +0200

    Fix for pdf page

--- app/pdf/[...slug]/page.tsx
+++ app/pdf/[...slug]/page.tsx
@@ -1,15 +1,24 @@
+import { Suspense } from 'react';
+
 type PDFProps = {
     params: Promise<{ slug: string[] }>;
 };
-export default async function PDFPage({ params }: PDFProps) {
+
+async function PDFIframe({ params }: PDFProps) {
     const { slug } = await params;
     slug.pop();
     const path = slug.join('/');
     const pdfUrl = `/api/pdf?path=${encodeURIComponent(path)}`;
 
+    return <iframe src={pdfUrl} style={{ width: '100%', height: '100%', border: 'none' }} />;
+}
+
+export default async function PDFPage({ params }: PDFProps) {
     return (
         <div style={{ height: '100vh' }}>
-            <iframe src={pdfUrl} style={{ width: '100%', height: '100%', border: 'none' }} />
+            <Suspense>
+                <PDFIframe params={params} />
+            </Suspense>
         </div>
     );
 }

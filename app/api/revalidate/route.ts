2e14aba6e11af3ca920a2df386f84a221bfb4b88
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-11-04 14:09:28 +0200

    Check the item

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from 'next/cache';
 
 export async function POST(request: NextRequest) {
     const { item } = await request.json();
+    console.log(item)
     const path = item.get.tree.path;
     
     if (path) {
